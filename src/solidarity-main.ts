03f2c14c68c55b1c3d841270ec7e770411c57916
simon0820s <simon0820s@gmail.com>
2024-09-25 02:53:02 -0500

    Initial commit

---
+++ src/solidarity-main.ts
@@ -0,0 +1,72 @@
+import {
+  RoleAdminChanged as RoleAdminChangedEvent,
+  RoleGranted as RoleGrantedEvent,
+  RoleRevoked as RoleRevokedEvent,
+  SolidarityGroupCreated as SolidarityGroupCreatedEvent
+} from "../generated/SolidarityMain/SolidarityMain"
+import {
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  SolidarityGroupCreated
+} from "../generated/schema"
+
+export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
+  let entity = new RoleAdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.previousAdminRole = event.params.previousAdminRole
+  entity.newAdminRole = event.params.newAdminRole
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleGranted(event: RoleGrantedEvent): void {
+  let entity = new RoleGranted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleRevoked(event: RoleRevokedEvent): void {
+  let entity = new RoleRevoked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleSolidarityGroupCreated(
+  event: SolidarityGroupCreatedEvent
+): void {
+  let entity = new SolidarityGroupCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.childSolidarityGroup = event.params.childSolidarityGroup
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
